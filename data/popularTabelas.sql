6dae21658e2d8d44b300fca5e2e2bc838884d955
Gabriel Costa <gabrielcosta@digix.com.br>
2021-06-10 23:37:15 -0400

    adiciona id no csv de data

--- data/popularTabelas.sql
+++ data/popularTabelas.sql
@@ -1,3 +1,3 @@
-COPY estado FROM './csv/estado.csv' WITH (FORMAT csv);
-COPY data FROM './csv/data.csv' WITH (FORMAT csv);
+COPY estado FROM '/data/estado.csv' WITH (FORMAT csv);
+COPY data FROM '/data/data.csv' WITH (FORMAT csv);
 -- COPY municipo FROM './csv/municipio.csv' WITH (FORMAT csv);
